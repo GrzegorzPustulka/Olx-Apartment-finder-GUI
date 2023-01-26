dc627e17a4d75a85d2f0f77aa2a5b794b1f19fbd
Grzegorz Pustulka <grzegorzpustulka@onet.pl>
2023-01-26 04:31:37 +0100

    splitting the code into two files

--- main.py
+++ main.py
@@ -3,87 +3,17 @@ from PyQt6.QtWidgets import QApplication, QWidget, \
 from PyQt6 import uic
 from PyQt6.QtGui import QPixmap, QFont
 import sys
-import requests
-from bs4 import BeautifulSoup
-import re
-import pandas as pd
-from dataclasses import dataclass
-import lxml
-import openpyxl
-
-@dataclass
-class Ads:
-    link: str
-    price: float
-    rent: float
-    total: float
+from scraping import scraping_olx
 
 
 def exit_application() -> None:
     sys.exit(0)
 
 
-def scraping(max, min, link, our_districts):
-    req = requests.get("https://www.olx.pl/d/nieruchomosci/mieszkania/krakow/")
-    soup = BeautifulSoup(req.text, 'lxml')
-    # count pages
-    count_pages = int(soup.select('li[data-testid="pagination-list-item"]')[3].text)
-
-    ad = soup.select("a.css-rc5s2u")
-    olx_ad = []
-    for name in ad:
-        if not "otodom" in name['href']:
-            olx_ad.append("https://www.olx.pl" + name['href'])
-        else:
-            olx_ad.append(name['href'])
-
-    # ad districts from olx 0-51
-    olx_districts = soup.find_all("p", attrs={"data-testid": "location-date"})
-
-    # ad price from olx 0-51
-    olx_buffer_prices = soup.find_all("p", attrs={"data-testid": "ad-price"})
-
-    # change prices from ad to friendly number
-    buffer = []
-    for i in olx_buffer_prices:
-        buffer.append(i.text)
-    text_prices = ''
-    for price in buffer:
-        text_prices += price
-    text_prices = text_prices.replace(' ', '').replace(',', '.')
-    olx_prices = [float(x) for x in re.findall(r'\d*\.\d+|\d+', text_prices)]
-
-    ads=[]
-    olx_rent = 0
-    for i, district in enumerate(olx_districts):
-        for name in our_districts:
-            if name in district.text:
-                if max >= olx_prices[i] >= min:
-                    req = requests.get(olx_ad[i])
-                    soup = BeautifulSoup(req.text, 'lxml')
-
-                    if "olx.pl" in olx_ad[i]:
-                        price_rent_buffer = soup.select('li.css-1r0si1e')
-                        for k in price_rent_buffer:
-                            if "Czynsz" in k.text:
-                                text_prices = k.text.replace(' ', '').replace(',', '.')
-                                olx_rent = (int(re.findall(r'\d+', text_prices)[0]))
-
-                    else:
-                        olx_rent = 0
-                    if max >= olx_prices[i] + olx_rent >= min:
-                        ad = Ads(olx_ad[i], olx_prices[i], olx_rent, olx_prices[i]+olx_rent)
-                        ads.append(ad)
-                    break
-    df = pd.DataFrame(ads)
-    print(df)
-    df.to_excel('OlxDane.xlsx')
-
-
-
 class Window(QWidget):
     def __init__(self):
         super().__init__()
+        self.roomRbtn = None
         self.searchLabel = None
         self.minPriceLabel = None
         self.maxPriceLabel = None
@@ -101,7 +31,6 @@ class Window(QWidget):
         self.selected_districts = None
         self.image_map = None
         uic.loadUi("MainWindow.ui", self)
-
         self.mapBtn.clicked.connect(self.open_map_image)
         self.exitBtn.clicked.connect(exit_application)
         self.searchBtn.clicked.connect(self.search_apartments)
@@ -162,12 +91,12 @@ class Window(QWidget):
             self.searchLabel.deleteLater()
             self.kindLayout.deleteLater()
             self.textDistrictsLabel.setText("Your filters: ")
-            # zmienic nazwe jeszcze
+            # change name
             self.textDistrictsLabel.setFixedSize(400, 50)
             self.textDistrictsLabel.setFont(QFont("Arial", 40))
             self.textDistrictsLabel.move(200, 30)
 
-            scraping(max_price, min_price, link, self.selected_districts)
+            scraping_olx(max_price, min_price, link, self.selected_districts)
 
 
 if __name__ == "__main__":
---
+++ scraping.py
@@ -0,0 +1,71 @@
+from dataclasses import dataclass
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+
+
+@dataclass
+class Ads:
+    link: str
+    price: float
+    rent: float
+    total: float
+
+
+def scraping_olx(max_price, min_price, link, our_districts):
+    req = requests.get("https://www.olx.pl/d/nieruchomosci/mieszkania/krakow/")
+    soup = BeautifulSoup(req.text, 'lxml')
+    # count pages
+    count_pages = int(soup.select('li[data-testid="pagination-list-item"]')[3].text)
+
+    ad = soup.select("a.css-rc5s2u")
+    olx_ad = []
+    for name in ad:
+        if "otodom" not in name['href']:
+            olx_ad.append("https://www.olx.pl" + name['href'])
+        else:
+            olx_ad.append(name['href'])
+
+    # ad districts from olx 0-51
+    olx_districts = soup.find_all("p", attrs={"data-testid": "location-date"})
+
+    # ad price from olx 0-51
+    olx_buffer_prices = soup.find_all("p", attrs={"data-testid": "ad-price"})
+
+    # change prices from ad to friendly number
+    buffer = []
+    for i in olx_buffer_prices:
+        buffer.append(i.text)
+    text_prices = ''
+    for price in buffer:
+        text_prices += price
+    text_prices = text_prices.replace(' ', '').replace(',', '.')
+    olx_prices = [float(x) for x in re.findall(r'\d*\.\d+|\d+', text_prices)]
+
+    ads = []
+    olx_rent = 0
+    for i, district in enumerate(olx_districts):
+        for name in our_districts:
+            if name in district.text:
+                if max_price >= olx_prices[i] >= min_price:
+                    req = requests.get(olx_ad[i])
+                    soup = BeautifulSoup(req.text, 'lxml')
+
+                    if "olx.pl" in olx_ad[i]:
+                        price_rent_buffer = soup.select('li.css-1r0si1e')
+                        for k in price_rent_buffer:
+                            if "Czynsz" in k.text:
+                                text_prices = k.text.replace(' ', '').replace(',', '.')
+                                olx_rent = (int(re.findall(r'\d+', text_prices)[0]))
+
+                    else:
+                        olx_rent = 0
+                    if max_price >= olx_prices[i] + olx_rent >= min_price:
+                        ad = Ads(olx_ad[i], olx_prices[i], olx_rent, olx_prices[i] + olx_rent)
+                        ads.append(ad)
+                    break
+    df = pd.DataFrame(ads)
+    print(df)
+    df.to_excel('OlxDane.xlsx')
